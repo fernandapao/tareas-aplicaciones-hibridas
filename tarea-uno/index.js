fb3a28cf2ebe4d49ba56ff7e2ce54ea8fdbe3f70
fernanda <fernanda.paoliello@davinci.edu.ar>
2024-08-29 09:51:44 -0300

    realice parte 2 y parte 3

--- tarea-uno/index.js
+++ tarea-uno/index.js
@@ -36,4 +36,70 @@ function task1(callback) {
   
   
   mainCallback();
+
+
+
+  //PARTE 2: PROMESAS
+
+function task1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Task 1 completa');
+      resolve('Task 1 resultado');
+    }, 1000);
+  });
+}
+
+function task2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Task 2 completa');
+      resolve('Task 2 resultado');
+    }, 2000);
+  });
+}
+
+function task3() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Task 3 completa');
+      resolve('Task 3 resultado');
+    }, 3000);
+  });
+}
+
+
+Promise.all([task1(), task2(), task3()])
+  .then((resultados) => {
+    console.log('Todas las task han sido completadas:', resultados);
+  })
+  .catch((error) => {
+    console.error('Error al completar las task:', error);
+  });
+
+
+
+  //PARTE 3: Async/Await
+
+async function mainAsync() {
+  try {
+    const result1 = await task1();
+    console.log(resultado1);
+    
+    const result2 = await task2();
+    console.log(resultado2);
+    
+    const result3 = await task3();
+    console.log(resultado3);
+    
+    console.log('Todas las task han sido completadas');
+  } catch (error) {
+    console.error('Error al ejecutar las task', error);
+  }
+}
+
+
+mainAsync();
+
+
   
